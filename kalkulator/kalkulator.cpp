dd38758ccd6dcc6c1bc48039c89a7752639a8c4b
sunne <smmmmxc@outlook.com>
2023-09-06 21:46:25 +0200

    p

--- kalkulator/kalkulator.cpp
+++ kalkulator/kalkulator.cpp
@@ -1,19 +1,4 @@
-//Make a C++ calculator application that can add, subtract, multiply, and divide.
-//The program should take in two numbers and an operator and output the answer.
-//The program should also have error handling in case the user does not enter a valid number or operator.
-//The program should be able to handle decimals and integers.
-//The program should be able to handle negative numbers.
-//The program should be able to handle division by zero.
-//The program should be able to handle invalid operators.
-//The program should be able to handle invalid input.
-//The program should be able to handle invalid number of inputs.
-//The program should be able to handle invalid number of operators.
-//The program should be able to handle invalid number of operands.
-//The program should be able to handle invalid number of operands and operators.
-//The program should be able to handle invalid number of operands and operators and inputs.
-//The program should be able to handle invalid number of operands and operators and inputs and numbers.
-//The program should be able to handle invalid number of operands and operators and inputs and numbers and operators.
-const int MAX = 100;
+
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -38,21 +23,18 @@ const int MAX = 100;
 #include <utility>
 #include <typeinfo>
 #include <locale>
-// Start coding the calculator
 using namespace std;
 int main()
 {
-	// Declare variables
 	double num1, num2, result;
 	char op;
-	// Ask user for input
+	
 	cout << "Wpisz pierwsza cyfre: ";
 	cin >> num1;
 	cout << "Wpisz operator arytmetyczny: ";
 	cin >> op;
 	cout << "Wpisz druga cyfre: ";
 	cin >> num2;
-	// Create switch statement to perform calculations
 	switch (op)
 	{
 	case '+':
@@ -71,7 +53,6 @@ int main()
 		cout << "Bledny operator arytmetyczny";
 		break;
 	}
-	// Output result
 	cout << "Wynik to " << result << endl;
 	return 0;
 
